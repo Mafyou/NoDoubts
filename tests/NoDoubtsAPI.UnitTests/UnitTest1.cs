a7ebaa34760773fdf8b5e4787d5c9d0694c1ad00
Mafyou <Sata77@gmail.com>
2025-06-28 02:01:10 +0200

    Add Tests projects

---
+++ tests/NoDoubtsAPI.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace NoDoubtsAPI.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/NoDoubtsWeb.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace NoDoubtsWeb.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
